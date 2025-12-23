73fa8a2903843ccf64f6364bc03e579d444f32a5
Sachindu Malshan <sachindu.info@gmail.com>
2025-12-23 23:33:41 +0530

    Development test environment

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def root():
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Health Metrics Service</title>
+    <title>Health Metrics Service - Development </title>
     <style>
         * {
             margin: 0;
